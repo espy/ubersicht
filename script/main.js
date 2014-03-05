b371ee12de3c46754948b3bf7bb6f4f8b837d49c
Benny Daon <bennydaon@gmail.com>
2014-03-06 00:06:24 +0200

    making it our own

--- script/main.js
+++ script/main.js
@@ -22,7 +22,7 @@
 $(function() {
   // The github user or organisation you'd like to load issues for
   // Defaults to hoodiehq!
-  var githubOrganisation = 'hoodiehq';
+  var githubOrganisation = 'hasadna';
   useHash()
   // labelForNewCommitters is what you label simple issues for new committers with
   // Will expose a new button "show issues for new committers" if not empty
